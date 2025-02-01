ff8b3592fa1f0104f249395f144317e221c9e51b
vijayoffl <56537792+vijayoffl@users.noreply.github.com>
2025-02-01 18:09:31 +0530

    Update database.py

--- app/database.py
+++ app/database.py
@@ -4,6 +4,7 @@ from sqlalchemy.orm import sessionmaker
 import os
 from dotenv import load_dotenv
 
+# Load environment variables from .env file
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
